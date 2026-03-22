d034b11c72cba5264bd9d585cab6650a9efafbbc
ZEUS 2 <cobrapk8ggg@gmail.com>
2026-03-22 20:31:01 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { HelmetProvider } from 'react-helmet-async';
 import Home from './src/screens/Home';
 import NovelPage from './src/screens/NovelPage';
 import Library from './src/screens/Library';
@@ -18,15 +19,17 @@ const queryClient = new QueryClient({
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/novel/:slug" element={<NovelPage />} />
-          <Route path="/library" element={<Library />} />
-          <Route path="/my-page" element={<MyPage />} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/novel/:slug" element={<NovelPage />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/my-page" element={<MyPage />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
